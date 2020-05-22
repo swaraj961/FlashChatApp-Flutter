39bc59c44576236ece48d2bf92c8f26aa0c42e34
Swaraj961 <swarajroutray961@gmail.com>
2020-05-22 09:47:28 +0530

    minor changes

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -27,150 +27,154 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
         resizeToAvoidBottomPadding: false,
         // backgroundColor: Color(0XFF4dd0e1).withOpacity(0.90),
-        body: ModalProgressHUD(
-          inAsyncCall: showspinner,
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: SingleChildScrollView(
+                  child: ModalProgressHUD(
+            inAsyncCall: showspinner,
+            child: Center(
+              child: SafeArea(
+                child: Column(
                   children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 50),
-                      child: Hero(
-                        tag: 'logo',
-                        child: Container(
-                          height: 200,
-                          child: Image.asset('images/logo.png'),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  'Welcome Back !',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 20,
-                      color: Colors.white70),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Container(
-                  height: 145,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(25)),
-                    color: Colors.orange.shade400.withOpacity(0.85),
-                  ),
-                  child: Form(
-                    key: formkey,
-                    child: Column(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Expanded(
-                          child: TextFormField(
-                            onChanged: (value) {
-                              email = value;
-                            },
-                            validator: (value) {
-                              if (!value.contains('@gmail.com')) {
-                                return 'Enter a valid Email';
-                              } else {
-                                return null;
-                              }
-                            },
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                                prefixIcon: Icon(
-                                  Icons.email,
-                                  color: Colors.white,
-                                ),
-                                border: InputBorder.none,
-                                hintText: '\tEnter your Email',
-                                hintStyle: TextStyle(
-                                    fontSize: 16, color: Colors.white),
-                                contentPadding: EdgeInsets.all(20)),
-                          ),
-                        ),
-                        Divider(
-                          thickness: 0.5,
-                        ),
-                        Expanded(
-                          child: TextFormField(
-                            onChanged: (value) {
-                              password = value;
-                            },
-                            validator: (value) {
-                              if (value.length < 6) {
-                                return 'Password must be atleast 6 characters';
-                              } else {
-                                return null;
-                              }
-                            },
-                            obscureText: visibletext,
-                            decoration: InputDecoration(
-                                suffixIcon: IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        visibletext = false;
-                                      });
-                                    },
-                                    icon: Icon(Icons.visibility)),
-                                prefixIcon: Icon(Icons.lock),
-                                border: InputBorder.none,
-                                hintText: "Password",
-                                hintStyle: TextStyle(
-                                    fontSize: 16, color: Colors.white),
-                                contentPadding: EdgeInsets.all(20)),
+                        Padding(
+                          padding: EdgeInsets.only(top: 50),
+                          child: Hero(
+                            tag: 'logo',
+                            child: Container(
+                              height: 200,
+                              child: Image.asset('images/logo.png'),
+                            ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Material(
-                  elevation: 15,
-                  color: Color(0xFF00e676),
-                  borderRadius: BorderRadius.circular(20),
-                  child: MaterialButton(
-                    onPressed: () async {
-                      setState(() {
-                        if (formkey.currentState.validate() == true) {
-                          showspinner = true;
-                        }
-                      });
-                      try {
-                        final newuser = await _auth.signInWithEmailAndPassword(
-                            email: email, password: password);
-                        if (newuser != null)
-                          Navigator.pushNamed(context, 'chat_page');
-                        setState(() {
-                          showspinner = false;
-                        });
-                      } catch (e) {}
-                    },
-                    minWidth: 150,
-                    height: 10,
-                    child: Text(
-                      'Login ',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 18),
+                    SizedBox(
+                      height: 15,
                     ),
-                  ),
+                    Text(
+                      'Welcome Back !',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 20,
+                          color: Colors.white70),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      height: 145,
+                      width: 350,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                        color: Colors.orange.shade400.withOpacity(0.85),
+                      ),
+                      child: Form(
+                        key: formkey,
+                        child: Column(
+                          children: <Widget>[
+                            Expanded(
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  email = value;
+                                },
+                                validator: (value) {
+                                  if (!value.contains('@gmail.com')) {
+                                    return 'Enter a valid Email';
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: InputDecoration(
+                                    prefixIcon: Icon(
+                                      Icons.email,
+                                      color: Colors.white,
+                                    ),
+                                    border: InputBorder.none,
+                                    hintText: '\tEnter your Email',
+                                    hintStyle: TextStyle(
+                                        fontSize: 16, color: Colors.white),
+                                    contentPadding: EdgeInsets.all(20)),
+                              ),
+                            ),
+                            Divider(
+                              thickness: 0.5,
+                            ),
+                            Expanded(
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  password = value;
+                                },
+                                validator: (value) {
+                                  if (value.length < 6) {
+                                    return 'Password must be atleast 6 characters';
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                obscureText: visibletext,
+                                decoration: InputDecoration(
+                                    suffixIcon: IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            visibletext = false;
+                                          });
+                                        },
+                                        icon: Icon(Icons.visibility)),
+                                    prefixIcon: Icon(Icons.lock),
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(
+                                        fontSize: 16, color: Colors.white),
+                                    contentPadding: EdgeInsets.all(20)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Material(
+                      elevation: 15,
+                      color: Color(0xFF00e676),
+                      borderRadius: BorderRadius.circular(20),
+                      child: MaterialButton(
+                        onPressed: () async {
+                          setState(() {
+                            if (formkey.currentState.validate() == true) {
+                              showspinner = true;
+                            }
+                          });
+                          try {
+                            final newuser = await _auth.signInWithEmailAndPassword(
+                                email: email, password: password);
+                            if (newuser != null)
+                              Navigator.pushNamed(context, 'chat_page');
+                            setState(() {
+                              showspinner = false;
+                            });
+                          } catch (e) {}
+                        },
+                        minWidth: 150,
+                        height: 10,
+                        child: Text(
+                          'Login ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-              ],
+              ),
             ),
           ),
         ));
--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -32,65 +32,73 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+   
       // backgroundColor:
       //     Color(0XFF4dd0e1).withOpacity(0.90), // withOpacity(controller.value),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: SafeArea(
-                  child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(8, 6, 15, 0),
-                child: Row(
-                  children: <Widget>[
-                    Hero(
-                      tag: 'logo',
-                      child: Container(
-                        child: Image.asset(
-                          'images/logo.png',
-                          height: animation1.value * 100,
+          padding: EdgeInsets.symmetric(horizontal: 30 ,vertical: 250),
+          child: SafeArea(
+                    child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(8, 6, 15, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Hero(
+                        tag: 'logo',
+                        child: Container(
+                          child: Image.asset(
+                            'images/logo.png',
+                            height: animation1.value * 100,
+                          ),
                         ),
                       ),
-                    ),
-                    TypewriterAnimatedTextKit(
-                      text: ['Flash Chat'],
-                      textAlign: TextAlign.start,
-                      speed: Duration(milliseconds: 555),
-                      textStyle: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 130,
-                    ),
-                  ],
+                      TypewriterAnimatedTextKit(
+                        text: ['Flash Chat'],
+                        textAlign: TextAlign.start,
+                        speed: Duration(milliseconds: 555),
+                        textStyle: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 130,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              RoundedButtons(
-                text: 'Login',
-                colour: Colors.blueAccent,
-                //Color(0xFF00e676),
-                onpressed: () {
-                  Navigator.pushNamed(context, 'login_page');
-                },
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              RoundedButtons(
-                text: 'Regsiter',
-                colour: Colors.redAccent,
-                onpressed: () {
-                  Navigator.pushNamed(context, 'register_page');
-                },
-              ),
-            ],
+                RoundedButtons(
+                  text: 'Login',
+                  colour: Colors.blueAccent,
+                  //Color(0xFF00e676),
+                  onpressed: () {
+                    Navigator.pushNamed(context, 'login_page');
+                  },
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                RoundedButtons(
+                  text: 'Regsiter',
+                  colour: Colors.redAccent,
+                  onpressed: () {
+                    Navigator.pushNamed(context, 'register_page');
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text('Made with ❤ Swaraj' ,textAlign: TextAlign.center,)
+              
+              ],
+          
+            ),
           ),
         ),
-      ),
+      
     );
   }
 }
